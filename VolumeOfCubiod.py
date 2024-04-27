d156669a852636427838542e85bc74bbc2af9f9f
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 14:16:44 +0545

    Create VolumeOfCubiod.py

---
+++ VolumeOfCubiod.py
@@ -0,0 +1,8 @@
+# write a program to ask length, breadth, and height of cuboid and display the volume of cubiod)
+
+length = int(input("Enter the length of cubioid: "))
+breadth = int(input("Enter the breadth of cubioid: "))
+Height = int(input("Enter the height of cubioid: "))
+ Volume = length * breadth * Height
+print(f"The volume of cubiod is {Volume}")
+
