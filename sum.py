4eb5e892d999e8a99a8643ba1a1a7451988985c8
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 14:07:27 +0545

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,7 @@
+# variables = to store data that can be accessed and modified during program execution. 
+#They can hold various types of data, including numbers, strings, and boolean values.
+#Variable names must follow certain rules, such as starting with a letter or underscore, and can contain letters, digits, and underscores.
+a = 1
+b = 2
+sum = a+b
+print(sum)
