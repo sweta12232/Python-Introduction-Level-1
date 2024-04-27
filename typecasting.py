8dc5574224147466dca48d731dfa566c20d7534c
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 14:13:24 +0545

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,20 @@
+'''Data Types:
+Integer (int): Represents whole numbers without any decimal points.
+Float (float): Represents numbers with decimal points.
+String (str): Represents sequences of characters enclosed within single or double quotes.
+Boolean (bool): Represents the values True or False, used for logical operations.'''
+
+a = 1
+b = 1.1
+c = "RAM"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#input in python
+a = input("enter the number:")
+print(type(a))
+a = int(input("enter the number:"))
+print(type(a))
