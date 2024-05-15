28796db0911b63858b4c65ae0eca95ea88554c61
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-05-15 20:55:31 +0545

    Create random.py

---
+++ random.py
@@ -0,0 +1,6 @@
+# The random module in Python helps us make things unpredictable in our code, like rolling dice or shuffling cards in a game. 
+# It lets us generate random numbers or pick random items from a list, adding an element of surprise or randomness to your program.
+
+import random
+randomNum = random.randint(1, 100)
+print("Random number:", randomNum)
